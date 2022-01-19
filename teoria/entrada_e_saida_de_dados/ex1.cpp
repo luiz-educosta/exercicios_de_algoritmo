dcab1cbe948cc9ccbe40ea42da889ed9849d482e
Luiz Eduardo Costa Rodrigues <luizeduardorodrigues@tai.inatel.br>
2022-01-18 22:30:47 -0300

    [Teoria/Cap1] Resolução do exercício 1.

---
+++ teoria/entrada_e_saida_de_dados/ex1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+/*Faça um código que mostre na tela a mensagem “Hello world”.*/
+
+int main(){
+
+    cout << "Hello world!\n";
+    
+    return 0;
+}
